11aa926cebcfd5ff854f949ab018784b43dda596
duanxiping <cicido@126.com>
2017-02-28 20:11:10 +0800

    去掉过滤代码，统计在分词中进行过滤

--- src/main/scala/wordseg/CreateDocVec.scala
+++ src/main/scala/wordseg/CreateDocVec.scala
@@ -25,11 +25,12 @@ object CreateDocVec {
       s"stat_date=${dt} order by trval desc limit 300000) b " +
       s"on a.word = b.word where a.stat_date=${dt}"
 
+    /*
     val stopWordsSQL = s"select distinct(word) from algo.dxp_label_stopwords"
     val selfDefinedStopWords = Array("先生","女士","大爷","小姐","某")
     val stopWordsArr = sparkEnv.hiveContext.sql(stopWordsSQL).
       map(_.getAs[String](0)).collect()
-
+    */
 
 
     val word2vecMap = sparkEnv.hiveContext.sql(wordsSQL).map(r => {
@@ -37,10 +38,10 @@ object CreateDocVec {
       val vec = r.getAs[String](1).split(",").map(_.toDouble)
       //val wordVec = Vectors.dense(vec)
       (word, vec)
-    }).collect().filterNot(r=>{
+    }).collect()/*.filterNot(r=>{
       stopWordsArr.contains(r._1) ||
       selfDefinedStopWords.filter(w=>r._1.contains(w)).length > 0
-    }).toMap
+    })*/.toMap
 
     println("*\n" * 30)
     println(word2vecMap.size)
@@ -50,7 +51,7 @@ object CreateDocVec {
       val id = r.getAs[String](0)
       val words = r.getAs[String](1).split(",").filter(word2vecMap.contains(_))
       (id,words)
-    }).filter(_._2.length > 5).map(r=>{
+    }).filter(_._2.length > 3).map(r=>{
       val oriDocVec = r._2.map(word2vecMap(_)).reduceLeft((a, b) => {
         a.zip(b).map(r => r._1 + r._2)
       })
